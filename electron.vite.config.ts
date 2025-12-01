1b0d7a76689b2a0f366d5de531a2378881526fb8
Anatoly Chernov <chertoly@gmail.com>
2025-12-02 00:48:49 +0200

    Add Civet

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,14 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import civetVitePlugin from '@danielx/civet/vite'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), civetVitePlugin({})]
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), civetVitePlugin({})]
   },
   renderer: {
     resolve: {
@@ -15,6 +16,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), civetVitePlugin({})]
   }
 })

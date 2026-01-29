0d00a969ca14bd92ac616602bc4065a8d30bc0fa
Anatoly Chernov <chertoly@gmail.com>
2026-01-29 18:40:43 +0200

    Compartmentalize keyboard events

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,14 +2,19 @@ import Versions from './components/Versions.civet'
 import Layout from './components/Layout'
 import electronLogo from './assets/electron.svg'
 
+import { Provider } from 'jotai'
+import store from './store'
+import './hotkeys'
+
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
+  // https://jotai.org/docs/guides/using-store-outside-react
   return (
-    <>
+    <Provider store={store}>
       <Layout />
       <Versions />
-    </>
+    </Provider>
   )
 }
 
